86fdafb137f6ed2bc7e40811780e498a41b25b00
g-adil-r <g69adil@gmail.com>
2026-07-12 13:49:52 +0700

    More display nominal setoran

--- app/src/main/java/com/example/proyeksp/ui/DaftarSetoranActivity.kt
+++ app/src/main/java/com/example/proyeksp/ui/DaftarSetoranActivity.kt
@@ -1,60 +1,40 @@
 package com.example.proyeksp.ui
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.proyeksp.R
-import com.example.proyeksp.database.Rekening
-import com.example.proyeksp.database.Transaksi
 import com.example.proyeksp.helper.CurrencyHelper
 import com.example.proyeksp.helper.DateHelper
 import com.example.proyeksp.ui.theme.MyTypography
@@ -98,12 +78,19 @@ fun DaftarSetoranScreen(viewModel: RekeningViewModel = viewModel()) {
                     imageView.setImageResource(R.drawable.logo_bumdes)
                 }
             )
+            Text(
+                text = stringResource(id = R.string.daftar_title),
+                fontSize = 24.sp,
+                style = MyTypography.textTitle,
+                softWrap = true,
+                modifier = Modifier.padding(top = 10.dp, bottom = 10.dp, start = 15.dp)
+            )
             if (rekeningList.isEmpty()) {
                 Box(
                     modifier = Modifier.fillMaxSize(),
                     contentAlignment = Alignment.Center
                 ) {
-                    Text(text = "Tidak ada data rekening...", style = MaterialTheme.typography.bodyLarge)
+                    Text(text = stringResource(id = R.string.no_data), style = MyTypography.textNormal)
                 }
             } else {
                 LazyColumn(
@@ -181,7 +168,7 @@ fun SetoranItem(
                     )
                 } else {
                     Text(
-                        text = "Belum ada setoran",
+                        text = stringResource(id = R.string.no_date_setor),
                         style = MyTypography.textNormal,
                         modifier = Modifier.padding(top = 4.dp),
                         color = Color.Red
